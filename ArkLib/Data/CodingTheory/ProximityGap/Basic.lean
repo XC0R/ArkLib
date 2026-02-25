2cc1f92c70017c1c6dbf74e1521c0d8e430fb5ec
Katerina Hristova <katherina.hristova@gmail.com>
2026-02-25 23:36:33 +0000

    cleanup of imports, variables and factor out trivariate

--- ArkLib/Data/CodingTheory/ProximityGap/Basic.lean
+++ ArkLib/Data/CodingTheory/ProximityGap/Basic.lean
@@ -5,29 +5,9 @@ Authors: Quang Dao, Katerina Hristova, František Silváši, Julian Sutherland,
          Ilia Vlasov, Chung Thai Nguyen
 -/
 
-import ArkLib.Data.CodingTheory.Basic
-import ArkLib.Data.CodingTheory.GuruswamiSudan
-import ArkLib.Data.CodingTheory.Prelims
-import ArkLib.Data.CodingTheory.ReedSolomon
 import ArkLib.Data.CodingTheory.InterleavedCode
-import ArkLib.Data.Polynomial.Bivariate
-import ArkLib.Data.Polynomial.RationalFunctions
 import ArkLib.Data.Probability.Notation
-import Mathlib.Algebra.Field.Basic
-import Mathlib.Algebra.Lie.OfAssociative
-import Mathlib.Algebra.Module.Submodule.Defs
-import Mathlib.Algebra.Polynomial.Basic
-import Mathlib.Data.Finset.BooleanAlgebra
-import Mathlib.Data.Real.Basic
-import Mathlib.Data.Real.Sqrt
-import Mathlib.Data.Set.Defs
-import Mathlib.FieldTheory.RatFunc.AsPolynomial
-import Mathlib.FieldTheory.Separable
-import Mathlib.LinearAlgebra.AffineSpace.AffineSubspace.Defs
 import Mathlib.Probability.Distributions.Uniform
-import Mathlib.RingTheory.Henselian
-import Mathlib.RingTheory.PowerSeries.Basic
-import Mathlib.RingTheory.PowerSeries.Substitution
 
 /-!
 # Proximity gap fundamental definitions
@@ -67,32 +47,31 @@ module codes over (scalar) rings.
 
 namespace ProximityGap
 
-open NNReal Finset Function
-open scoped ProbabilityTheory
-open scoped BigOperators LinearCode
-open Code Affine
+open NNReal Finset Function Code Affine
+open scoped ProbabilityTheory BigOperators LinearCode Affine
 
 universe u v w k l
 
-open scoped Affine
 section CoreSecurityDefinitions
+
+
 variable {ι : Type*} [Fintype ι] [DecidableEq ι]
 variable {κ : Type k} {ι : Type l} [Fintype κ] [Fintype ι] [Nonempty ι]
 -- κ => row indices, ι => column indices
-variable {F : Type v} [Ring F] [Fintype F] [DecidableEq F]
+variable {F : Type v} [Ring F] [Fintype F]
 -- variable {M : Type} [Fintype M] -- Message space type
 variable {A : Type w} [Fintype A] [DecidableEq A] [AddCommMonoid A] [Module F A] -- Alphabet type
 variable (C : Set (ι → A))
 
 /-- The proximity measure of two vectors `u` and `v` from a code `C` at distance `d` is the number
-  of vectors at distance at most `d` from the linear combination of `u` and `v` with coefficients
-  `r` in `F`. -/
+of vectors at distance at most `d` from the linear combination of `u` and `v` with coefficients
+`r` in `F`. -/
 noncomputable def proximityMeasure (u v : Word A ι) (d : ℕ) : ℕ :=
   Fintype.card {r : F | Δ₀(r • u + (1 - r) • v, C) ≤ d}
 
 /-- A code `C` exhibits proximity gap at distance `d` and cardinality bound `bound` if for every
-      pair of vectors `u` and `v`, whenever the proximity measure for `C u v d` is greater than
-      `bound`, then the distance of `[u | v]` from the interleaved code `C ^⊗ 2` is at most `d`. -/
+pair of vectors `u` and `v`, whenever the proximity measure for `C u v d` is greater than
+`bound`, then the distance of `[u | v]` from the interleaved code `C ^⊗ 2` is at most `d`. -/
 def proximityGap (d : ℕ) (bound : ℕ) : Prop :=
   ∀ u v : Word (A := A) (ι := ι), (proximityMeasure (F := F) C u v d > bound)
     →
@@ -100,7 +79,7 @@ def proximityGap (d : ℕ) (bound : ℕ) : Prop :=
     (Δ₀(u ⋈₂ v, C ^⋈ (Fin 2)) ≤ d)
 
 variable {ι : Type} [Fintype ι] [Nonempty ι] [DecidableEq ι]
-  {F : Type} [Ring F] [Fintype F] [DecidableEq F]
+  {F : Type} [Ring F] [Fintype F]
   {k : ℕ}
 
 /-- Definition 1.1 in [BCIKS20].
@@ -126,8 +105,7 @@ For every pair of words `u₀, u₁`, if the probability that a random affine li
 `δ`-close to `C` exceeds `ε`, then `u₀` and `u₁` have correlated agreement with `C`.
 -- **TODO**: prove that `δ_ε_correlatedAgreementAffineLines` implies `δ_ε_proximityGap`
 -/
-noncomputable def δ_ε_correlatedAgreementAffineLines
-    {ι : Type*} [Fintype ι] [Nonempty ι] [DecidableEq ι] [Module F A]
+noncomputable def δ_ε_correlatedAgreementAffineLines [Module F A]
     (C : Set (ι → A)) (δ ε : ℝ≥0) : Prop :=
   ∀ (u : WordStack (A := A) (κ := Fin 2) (ι := ι)),
     Pr_{let z ← $ᵖ F}[δᵣ(u 0 + z • u 1, C) ≤ δ] > ε →
@@ -138,8 +116,7 @@ with respect to the proximity parameter `δ` and the error bound `ε`, folding d
 ∀ word stack `u` of size `2^ϑ`, if the probability that
   (a random multilinear combination of the word stack `u` with randomness `r` is `δ`-close to `C`)
   exceeds `ε`, then the word stack `u` has correlated agreement with `C ^⋈ (2^ϑ)`. -/
-def δ_ε_multilinearCorrelatedAgreement [CommRing F]
-  {ι : Type*} [Fintype ι] [Nonempty ι] [DecidableEq ι] [Module F A]
+def δ_ε_multilinearCorrelatedAgreement [CommRing F] [Module F A]
   (C : Set (ι → A)) (ϑ : ℕ) (δ ε : ℝ≥0) : Prop :=
   ∀ (u : WordStack A (Fin (2^ϑ)) ι),
     Pr_{let r ← $ᵖ (Fin ϑ → F)}[ -- This syntax only works with (A : Type 0)
@@ -161,7 +138,7 @@ have correlated agreement.
 noncomputable def δ_ε_correlatedAgreementCurves {k : ℕ}
     {A : Type 0} [AddCommMonoid A] [Module F A] [Fintype A] [DecidableEq A]
     (C : Set (ι → A)) (δ ε : ℝ≥0) : Prop :=
-  ∀ (u : WordStack (A := A) (κ := Fin (k + 1)) (ι := ι)),
+    ∀ (u : WordStack (A := A) (κ := Fin (k + 1)) (ι := ι)),
     Pr_{let y ← $ᵖ (Curve.polynomialCurveFinite (F := F) (A := A) u)}[ δᵣ(y.1, C) ≤ δ ] > k * ε
       → jointAgreement (F := A) (κ := Fin (k + 1)) (ι := ι) (C := C) (W := u) (δ := δ)
 
@@ -173,36 +150,12 @@ exceeds `ε`, then the words `u₀, u₁, ..., uₖ` have correlated agreement. 
 noncomputable def δ_ε_correlatedAgreementAffineSpaces
     {A : Type 0} [AddCommGroup A] [Module F A] [Fintype A] [DecidableEq A]
     (C : Set (ι → A)) (δ ε : ℝ≥0) : Prop :=
-  ∀ (u : WordStack (A := A) (κ := Fin (k + 1)) (ι := ι)),
+    ∀ (u : WordStack (A := A) (κ := Fin (k + 1)) (ι := ι)),
     Pr_{let y ← $ᵖ (affineSubspaceAtOrigin (F := F) (u 0) (Fin.tail u))}[ δᵣ(y.1, C) ≤ δ ] > ε →
     jointAgreement (F := A) (κ := Fin (k + 1)) (ι := ι) (C := C) (W := u) (δ := δ)
 
 end CoreSecurityDefinitions
 
-namespace Trivariate
-
-variable {F : Type} [Field F] [DecidableEq F] [DecidableEq (RatFunc F)]
-
-open Polynomial Bivariate
-
-noncomputable def eval_on_Z₀ (p : (RatFunc F)) (z : F) : F :=
-  RatFunc.eval (RingHom.id _) z p
-
-notation3:max R "[Z][X]" => Polynomial (Polynomial R)
-
-notation3:max R "[Z][X][Y]" => Polynomial (Polynomial (Polynomial (R)))
-
-notation3:max "Y" => Polynomial.X
-notation3:max "X" => Polynomial.C Polynomial.X
-notation3:max "Z" => Polynomial.C (Polynomial.C Polynomial.X)
-
-noncomputable opaque eval_on_Z (p : F[Z][X][Y]) (z : F) : F[X][Y] :=
-  p.map (Polynomial.mapRingHom (Polynomial.evalRingHom z))
-
-open Polynomial.Bivariate in
-noncomputable def toRatFuncPoly (p : F[Z][X][Y]) : (RatFunc F)[X][Y] :=
-  p.map (Polynomial.mapRingHom (algebraMap F[X] (RatFunc F)))
-
-end Trivariate
-
 end ProximityGap
+
+#min_imports
