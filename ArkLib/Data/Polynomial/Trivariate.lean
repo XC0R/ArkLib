6df5fc95483cded3ff9708144a174c8197fc6c5d
Katerina Hristova <katherina.hristova@gmail.com>
2026-02-25 23:31:26 +0000

    Factor out the trivariate notation and lemmas

---
+++ ArkLib/Data/Polynomial/Trivariate.lean
@@ -0,0 +1,61 @@
+/-
+Copyright (c) 2024-2025 ArkLib Contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Katerina Hristova, František Silváši, Julian Sutherland, Ilia Vlasov
+-/
+
+import ArkLib.Data.Polynomial.Bivariate
+import Mathlib.FieldTheory.RatFunc.AsPolynomial
+
+/-!
+# Short interface on Trivariate Polynomials
+
+We define trivariate polynomials to match their representation in statements and proofs of [BCIKS20]
+
+## Main Definitions
+
+### Notation for trivariate polynomials and evaluation homomorphisms
+- `proximityMeasure`: Counts vectors close to linear combinations with code `C`
+- `proximityGap`: Proximity gap property at distance `d` with cardinality bound
+- `δ_ε_proximityGap`: Generic `(δ, ε)`-proximity gap for any collection of sets
+
+## References
+
+- [BCIKS20] Eli Ben-Sasson, Dan Carmon, Yuval Ishai, Swastik Kopparty, and Shubhangi Saraf.
+  Proximity gaps for Reed–Solomon codes. In 2020 IEEE 61st Annual Symposium on Foundations of
+  Computer Science (FOCS), 2020. Full paper: https://eprint.iacr.org/2020/654,
+  version 20210703:203025.
+
+-/
+
+namespace Trivariate
+
+variable {F : Type} [Field F] [DecidableEq (RatFunc F)]
+
+open Polynomial Bivariate
+
+/-- Evaluate a rational function on a point. -/
+noncomputable def eval_on_Z₀ (p : (RatFunc F)) (z : F) : F :=
+  RatFunc.eval (RingHom.id _) z p
+
+notation3:max R "[Z][X]" => Polynomial (Polynomial R)
+
+notation3:max R "[Z][X][Y]" => Polynomial (Polynomial (Polynomial R))
+
+notation3:max "Y" => Polynomial.X
+notation3:max "X" => Polynomial.C Polynomial.X
+notation3:max "Z" => Polynomial.C (Polynomial.C Polynomial.X)
+
+/-- A ring homomorphism mapping a trivariate polynomial with coefficients in `F` and variables
+`Z, X, Y` to a bivariate polynomial with coeffiecients in `F` and variables in `X, Y` by evaluating
+on a point `z ∈ F`. -/
+noncomputable opaque eval_on_Z (p : F[Z][X][Y]) (z : F) : F[X][Y] :=
+  p.map (Polynomial.mapRingHom (Polynomial.evalRingHom z))
+
+open Polynomial.Bivariate in
+/-- A ring homomorphism mapping a trivariate polynomial to a element in the field of rational
+functions of polynomial ring in two variables. -/
+noncomputable def toRatFuncPoly (p : F[Z][X][Y]) : (RatFunc F)[X][Y] :=
+  p.map (Polynomial.mapRingHom (algebraMap F[X] (RatFunc F)))
+
+end Trivariate
